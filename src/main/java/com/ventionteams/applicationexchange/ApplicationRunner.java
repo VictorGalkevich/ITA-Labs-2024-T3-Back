1f9c9f2e4c582245c443195ad63bbcc2a340f6be
VictorGalkevich <2vektor54@gmail.com>
2024-01-23 17:07:30 +0300

    created base project

--- src/main/java/com/ventionteams/applicationexchange/ApplicationRunner.java
+++ src/main/java/com/ventionteams/applicationexchange/ApplicationRunner.java
@@ -1,11 +1,13 @@
-package com.ventionteams;
+package com.ventionteams.applicationexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationRunner {
+
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
     }
+
 }

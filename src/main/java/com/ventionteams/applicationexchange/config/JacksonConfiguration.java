4945921e88cb3b5e20f47db7a55ef75e0b54b6ec
VictorGalkevich <2vektor54@gmail.com>
2024-01-25 17:19:44 +0300

    removed gson dependency

--- src/main/java/com/ventionteams/applicationexchange/config/JacksonConfiguration.java
+++ src/main/java/com/ventionteams/applicationexchange/config/JacksonConfiguration.java
@@ -1,12 +1,11 @@
 package com.ventionteams.applicationexchange.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfiguration {
+public class JacksonConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();

602f32838758337f1afd2af626003b9dcf2b575a
VictorGalkevich <2vektor54@gmail.com>
2024-01-31 18:02:35 +0300

    test deleted

--- src/test/java/com/ventionteams/applicationexchange/ApplicationRunnerTests.java
+++ src/test/java/com/ventionteams/applicationexchange/ApplicationRunnerTests.java
@@ -1,15 +1,9 @@
 package com.ventionteams.applicationexchange;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ApplicationexchangeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+class ApplicationRunnerTests {
 }

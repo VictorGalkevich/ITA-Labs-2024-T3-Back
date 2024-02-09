174ae170cbc38edb941e0fec8e529434ec8264a6
VictorGalkevich <2vektor54@gmail.com>
2024-02-09 21:11:06 +0300

    added role validation

--- src/main/java/com/ventionteams/applicationexchange/dto/UserCreateEditDto.java
+++ src/main/java/com/ventionteams/applicationexchange/dto/UserCreateEditDto.java
@@ -2,10 +2,7 @@ package com.ventionteams.applicationexchange.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ventionteams.applicationexchange.entity.enumeration.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record UserCreateEditDto(
         @JsonProperty("first_name")
@@ -16,6 +13,7 @@ public record UserCreateEditDto(
         String lastName,
         @Email
         String email,
+        @NotNull
         Role role,
         @Size(min = 10)
         String password,

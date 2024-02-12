c99c8edb6810a46942558f3855203fb2932eb000
VictorGalkevich <2vektor54@gmail.com>
2024-02-12 18:24:35 +0300

    deleted unused stuff

--- src/main/java/com/ventionteams/applicationexchange/repository/UserRepository.java
+++ src/main/java/com/ventionteams/applicationexchange/repository/UserRepository.java
@@ -7,5 +7,4 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String username);
 }

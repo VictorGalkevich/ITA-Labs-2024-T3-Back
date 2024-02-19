5aaf4d79160c870d247564774dd5e26c8eb6e0aa
VictorGalkevich <2vektor54@gmail.com>
2024-02-19 14:42:55 +0300

    removed okta

--- src/main/java/com/ventionteams/applicationexchange/config/SecurityConfig.java
+++ src/main/java/com/ventionteams/applicationexchange/config/SecurityConfig.java
@@ -1,19 +1,15 @@
 package com.ventionteams.applicationexchange.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.security.config.Customizer.*;
@@ -22,12 +18,6 @@ import static org.springframework.security.config.Customizer.*;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${okta.oauth2.issuer}")
-    private String issuer;
-    @Value("${okta.oauth2.client-id}")
-    private String clientId;
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -35,21 +25,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-                .oauth2Login(withDefaults())
-                .logout(logout -> logout
-                        .addLogoutHandler(logoutHandler()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
         return http.build();
-    }
-
-    private LogoutHandler logoutHandler() {
-        return (request, response, authentication) -> {
-            try {
-                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        };
     }
 
     @Bean

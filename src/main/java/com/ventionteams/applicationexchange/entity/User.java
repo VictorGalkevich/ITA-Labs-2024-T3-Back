34e71919abdc1d4bf0afa521a0f89219b1524ec0
karina_kuiko <KarinaIhorevna@gmail.com>
2024-03-25 19:47:11 +0300

    fix assigning user's lot

--- src/main/java/com/ventionteams/applicationexchange/entity/User.java
+++ src/main/java/com/ventionteams/applicationexchange/entity/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
